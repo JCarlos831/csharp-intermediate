97c30bf4093edfbc8fe4e59d4aea8afdace68346
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-06-12 12:48:14 -0400

    Section 3 videos completed.

---
+++ Section3/Inheritance/Inheritance/Text.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Inheritance
+{
+    public class Text : PresentaionObject
+    {
+        public int FontSize
+        {
+            get;
+            set;
+        }
+
+        public string FontName
+        {
+            get;
+            set;
+        }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine("We added a link to " + url);
+        }
+    }
+}
