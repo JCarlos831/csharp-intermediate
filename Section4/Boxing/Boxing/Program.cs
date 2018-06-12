51decd68119ae1058238e15846b098cb2253bb5b
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-06-12 18:30:25 -0400

    Section 5 videos completed

---
+++ Section4/Boxing/Boxing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Boxing
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Juan");
+            list.Add(DateTime.Today);
+
+            var number = (int)list[1];
+
+            var anotherList = new List<int>();
+            var names = new List<string>();
+            names.Add();
+        }
+    }
+}
