ed56767bad002c75f875218ef87cc826cc9be981
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-06-12 11:12:27 -0400

    Section 2 videos completed.

---
+++ Section2/Properties/Properties/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Properties
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1983, 8, 31));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
