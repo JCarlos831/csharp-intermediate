8f3c5383dbaae9a62b99a7426e32c12ab0d9df94
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-06-14 12:01:35 -0400

    Abstract Classes and Members video

---
+++ Section5/AbstractClasses/AbstractClasses/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AbstractClasses
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+//            var shape = new Shape();
+            
+            var circle = new Circle();
+            circle.Draw();
+
+            var rectangle = new Rectangle();
+            rectangle.Draw();
+        }
+    }
+}
