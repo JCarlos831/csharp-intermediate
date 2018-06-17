b5c9a951203d4dc33b93065393ae7365ce8e6e2d
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-06-17 12:28:38 -0400

    Finished Interfaces and Polymorphism Video.

---
+++ Section6/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var encoder = new VideoEncoder();
+            encoder.RegisterNotificationChannel(new MailNotifiactionChannel());
+            encoder.RegisterNotificationChannel(new SmsNotifiactionChannel());
+            encoder.Encode(new Video());
+        }
+    }
+}
